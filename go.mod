42b320d15384e6e55eee001e9294222032fc7bc4
futuretea <1913508671@qq.com>
2021-05-25 22:34:09 +0800

    add code

---
+++ go.mod
@@ -0,0 +1,61 @@
+module github.com/futuretea/harvester-inventory
+
+go 1.15
+
+replace (
+	github.com/dgrijalva/jwt-go => github.com/dgrijalva/jwt-go v3.2.1-0.20200107013213-dc14462fd587+incompatible
+	github.com/docker/distribution => github.com/docker/distribution v0.0.0-20191216044856-a8371794149d
+	github.com/docker/docker => github.com/docker/docker v1.4.2-0.20200203170920-46ec8731fbce
+	github.com/googleapis/gnostic => github.com/googleapis/gnostic v0.4.1
+	github.com/knative/pkg => github.com/rancher/pkg v0.0.0-20190514055449-b30ab9de040e
+	github.com/openshift/api => github.com/openshift/api v0.0.0-20191219222812-2987a591a72c
+	github.com/openshift/client-go => github.com/openshift/client-go v0.0.0-20191125132246-f6563a70e19a
+	github.com/operator-framework/operator-lifecycle-manager => github.com/operator-framework/operator-lifecycle-manager v0.0.0-20190128024246-5eb7ae5bdb7a
+	github.com/rancher/rancher/pkg/apis => github.com/rancher/rancher/pkg/apis v0.0.0-20210304063736-65f7c844267b
+	github.com/rancher/rancher/pkg/client => github.com/rancher/rancher/pkg/client v0.0.0-20210304063736-65f7c844267b
+
+	k8s.io/api => k8s.io/api v0.20.4
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.20.4
+	k8s.io/apimachinery => k8s.io/apimachinery v0.20.4
+	k8s.io/apiserver => k8s.io/apiserver v0.20.4
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.20.4
+	k8s.io/client-go => k8s.io/client-go v0.20.4
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.20.4
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.20.4
+	k8s.io/code-generator => k8s.io/code-generator v0.20.4
+	k8s.io/component-base => k8s.io/component-base v0.20.4
+	k8s.io/component-helpers => k8s.io/component-helpers v0.20.4
+	k8s.io/controller-manager => k8s.io/controller-manager v0.20.4
+	k8s.io/cri-api => k8s.io/cri-api v0.20.4
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.20.4
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.20.4
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.20.4
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.20.4
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.20.4
+	k8s.io/kubectl => k8s.io/kubectl v0.20.4
+	k8s.io/kubelet => k8s.io/kubelet v0.20.4
+	k8s.io/kubernetes => k8s.io/kubernetes v1.20.2
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.20.4
+	k8s.io/metrics => k8s.io/metrics v0.20.4
+	k8s.io/mount-utils => k8s.io/mount-utils v0.20.4
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.20.4
+
+	kubevirt.io/client-go => github.com/kubevirt/client-go v0.40.0-rc.2
+	kubevirt.io/containerized-data-importer => github.com/rancher/kubevirt-containerized-data-importer v1.26.1-0.20210303063201-9e7a78643487
+	sigs.k8s.io/structured-merge-diff => sigs.k8s.io/structured-merge-diff v0.0.0-20190302045857-e85c7b244fd2
+)
+
+require (
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/harvester/harvester v0.2.0
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/rancher/wrangler v0.7.3-0.20210219161540-ef7fe9ce2443
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/net v0.0.0-20210326060303-6b1517762897 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	k8s.io/apimachinery v0.20.4
+	k8s.io/client-go v12.0.0+incompatible
+	k8s.io/klog v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	harvclient "github.com/harvester/harvester/pkg/generated/clientset/versioned"
+	"github.com/rancher/wrangler/pkg/kubeconfig"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog"
+)
+
+const (
+	HostIPSourceType = "InternalIP"
+
+	AnsibleVarHost = "ansible_host"
+	AnsibleVarUser = "ansible_user"
+
+	FieldHostVars = "hostvars"
+	FieldMeta     = "_meta"
+	FieldChildren = "children"
+	FieldHosts    = "hosts"
+
+	GroupAll             = "all"
+	GroupNodes           = "nodes"
+	GroupVirtualMachines = "vms"
+
+	HarvesterSSHUserLabel = "harvesterhci.io/ssh-user"
+
+	defaultSSHUser = "rancher"
+)
+
+type Host struct {
+	Name        string
+	IP          string
+	Kind        string
+	Labels      map[string]string
+	Annotations map[string]string
+}
+
+type Inventory struct {
+	Data map[string]interface{}
+}
+
+func (i *Inventory) AddGroupHosts(groupName string, hostNames ...string) {
+	i.AddGroupFiled(groupName, FieldHosts, hostNames...)
+}
+
+func (i *Inventory) AddGroupChildren(groupName string, groupNames ...string) {
+	i.AddGroupFiled(groupName, FieldChildren, groupNames...)
+}
+
+func (i *Inventory) AddGroupFiled(groupName string, filed string, names ...string) {
+	var group map[string][]string
+	if i.Data[groupName] == nil {
+		group = map[string][]string{
+			filed: {},
+		}
+	} else {
+		group = i.Data[groupName].(interface{}).(map[string][]string)
+	}
+	for _, name := range names {
+		group[filed] = append(group[filed], name)
+	}
+	i.Data[groupName] = group
+}
+
+func (i *Inventory) AddMetaHost(hostName string, hostVars ...map[string]string) {
+	host := i.Data[FieldMeta].(interface{}).(map[string]map[string]map[string]string)[FieldHostVars][hostName]
+	if host == nil {
+		host = make(map[string]string)
+	}
+	for _, hostVar := range hostVars {
+		for k, v := range hostVar {
+			host[k] = v
+		}
+	}
+	i.Data[FieldMeta].(map[string]map[string]map[string]string)[FieldHostVars][hostName] = host
+}
+
+func (i *Inventory) String() string {
+	output, err := json.Marshal(i.Data)
+	if err != nil {
+		klog.Fatalln(err)
+	}
+	return string(output)
+}
+
+func NewInventory() *Inventory {
+	return &Inventory{
+		Data: map[string]interface{}{
+			FieldMeta: map[string]map[string]map[string]string{
+				FieldHostVars: {},
+			},
+			GroupAll: map[string][]string{
+				FieldChildren: {},
+			},
+		},
+	}
+}
+
+func generateGroupName(kind, prefix, key, value string) string {
+	groupName := kind + "_" + prefix + "_" + key + "_" + value
+	groupName = strings.ReplaceAll(groupName, ".", "_")
+	groupName = strings.ReplaceAll(groupName, "-", "_")
+	groupName = strings.ReplaceAll(groupName, "/", "_")
+	return groupName
+}
+
+func autoGroupLabel(labelName string) bool {
+	return strings.Contains(labelName, "kubernetes") || strings.Contains(labelName, "k3s") || strings.Contains(labelName, "harvester")
+}
+
+func parserHost(host Host) (map[string]string, []string) {
+	vars := map[string]string{
+		AnsibleVarHost: host.IP,
+		AnsibleVarUser: defaultSSHUser,
+	}
+
+	if v, ok := host.Annotations[HarvesterSSHUserLabel]; ok {
+		vars[AnsibleVarUser] = v
+	}
+
+	var groupNames []string
+	switch host.Kind {
+	case "node":
+		groupNames = append(groupNames, GroupNodes)
+	case "vm":
+		groupNames = append(groupNames, GroupVirtualMachines)
+	}
+	for key, value := range host.Labels {
+		if !autoGroupLabel(key) {
+			continue
+		}
+		groupName := generateGroupName(host.Kind, "label", key, value)
+		groupNames = append(groupNames, groupName)
+	}
+	return vars, groupNames
+}
+
+func buildInventory(hosts []Host) string {
+	inventory := NewInventory()
+	for _, host := range hosts {
+		vars, groupNames := parserHost(host)
+		inventory.AddMetaHost(host.Name, vars)
+		for _, groupName := range groupNames {
+			inventory.AddGroupHosts(groupName, host.Name)
+		}
+	}
+	inventory.AddGroupChildren(GroupAll, GroupNodes, GroupVirtualMachines)
+	return inventory.String()
+}
+
+func getAllVMs(harvClientSet *harvclient.Clientset) []Host {
+	vmiList, err := harvClientSet.KubevirtV1().VirtualMachineInstances("default").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+	var (
+		ip  string
+		kind = "vm"
+		result = make([]Host, 0, len(vmiList.Items))
+	)
+
+	for _, vmi := range vmiList.Items {
+		for _, networkInterface := range vmi.Status.Interfaces {
+			if networkInterface.IP != "" {
+				ip = networkInterface.IP
+				break
+			}
+		}
+		result = append(result, Host{
+			Kind:        kind,
+			Name:        kind + "-" + vmi.Name,
+			Labels:      vmi.Labels,
+			Annotations: vmi.Annotations,
+			IP:          ip,
+		})
+	}
+	return result
+}
+
+func getAllNode(kubeClientSet *clientset.Clientset) []Host {
+	nodeClient := kubeClientSet.CoreV1().Nodes()
+	nodes, err := nodeClient.List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+	var (
+		kind = "node"
+		result = make([]Host, 0, len(nodes.Items))
+	)
+
+	for _, node := range nodes.Items {
+		nodeIP := ""
+		for _, address := range node.Status.Addresses {
+			if address.Type == HostIPSourceType {
+				nodeIP = address.Address
+				break
+			}
+		}
+		result = append(result, Host{
+			Kind:        kind,
+			Name:        kind + "-" + node.Name,
+			Labels:      node.Labels,
+			Annotations: node.Annotations,
+			IP:          nodeIP,
+		})
+	}
+	return result
+}
+
+func getRestConfig() *rest.Config {
+	clientConfig := kubeconfig.GetNonInteractiveClientConfig("")
+	restConfig, err := clientConfig.ClientConfig()
+	if err != nil {
+		klog.Fatalln(err)
+	}
+	return restConfig
+}
+
+func main() {
+	restConfig := getRestConfig()
+	kubeClientSet := clientset.NewForConfigOrDie(restConfig)
+	nodeList := getAllNode(kubeClientSet)
+	harvClientSet := harvclient.NewForConfigOrDie(restConfig)
+	vmiList := getAllVMs(harvClientSet)
+	hosts := make([]Host, 0, len(nodeList)+len(vmiList))
+	hosts = append(hosts, nodeList...)
+	hosts = append(hosts, vmiList...)
+	fmt.Println(buildInventory(hosts))
+}
